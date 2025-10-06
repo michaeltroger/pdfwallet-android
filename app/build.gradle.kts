a849c9c4bef5256ee92886fce9ddf899a85fa41f
Michael Troger <11340859+michaeltroger@users.noreply.github.com>
2025-10-06 22:14:40 +0200

    Silent release for fdroid (#339)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
 
     defaultConfig {
         applicationId = "com.michaeltroger.gruenerpass"
-        versionCode = 61
-        versionName = "4.4.1"
+        versionCode = 62
+        versionName = "4.4.2"
     }
 }
 
