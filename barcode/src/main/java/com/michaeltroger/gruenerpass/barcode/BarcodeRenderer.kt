4e31c89bb021098a39bff22a6737d3779744712c
Michael Troger <11340859+michaeltroger@users.noreply.github.com>
2026-02-16 23:05:05 +0100

    Properly interpret GS1 as text

--- barcode/src/main/java/com/michaeltroger/gruenerpass/barcode/BarcodeRenderer.kt
+++ barcode/src/main/java/com/michaeltroger/gruenerpass/barcode/BarcodeRenderer.kt
@@ -119,10 +119,14 @@ internal class BarcodeRendererImpl @Inject constructor(
     }
 
     private fun encodeBarcodeAsBitmap(extractedCode: ZxingCpp.Result): Bitmap {
-        val content = if (extractedCode.contentType == ZxingCpp.ContentType.TEXT) {
-            extractedCode.text
-        } else {
-            extractedCode.rawBytes
+        val content = when (extractedCode.contentType) {
+            ZxingCpp.ContentType.TEXT,
+            ZxingCpp.ContentType.GS1 -> {
+                extractedCode.text
+            }
+            else -> {
+                extractedCode.rawBytes
+            }
         }
 
         return ZxingCpp.encodeAsBitmap(
