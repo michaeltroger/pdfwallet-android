c7644c123cf3e8cf143e15991d9d8b33cdbc4c9d
Michael Troger <11340859+michaeltroger@users.noreply.github.com>
2026-03-05 00:20:03 +0100

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
 
     defaultConfig {
         applicationId = "com.michaeltroger.gruenerpass"
-        versionCode = 76
-        versionName = "5.1.1"
+        versionCode = 77
+        versionName = "5.1.2"
     }
 }
 
